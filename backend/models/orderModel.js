da7ebdb40502cf16dd836b8fa421b0d517efe6fd
Subhashis Das <109717131+subh93@users.noreply.github.com>
2023-05-10 19:20:19 +0530

    The project of Food Delivery Website using MERN

---
+++ backend/models/orderModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const orderSchema = mongoose.Schema({
+    name: { type: String, required:true },
+    email: { type: String, required:true },
+    address: { type: String, required:true },
+    orderItems: [],
+    orderAmount: {type: Number, required: true},
+    isDeliverd: {type: Boolean, required: true, default: false},
+    transactionid: { type: String, required:true },
+    mode_of_payment: { type: String, default: 'cash on delivery' }
+},  { timestamp: true, versionKey:false, collection: 'orderlist' })
+
+module.exports = mongoose.model('orderlist', orderSchema)
