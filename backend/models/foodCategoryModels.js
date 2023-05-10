d9dc343559a5cbb390a222cb121356fdecfb3198
Subhashis Das <109717131+subh93@users.noreply.github.com>
2023-05-10 19:13:28 +0530

    Delete the older files

--- backend/models/foodCategoryModels.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const foodCategorySchema = mongoose.Schema({
-    CategoryName: { type: String, required:true }
-}, { timestamp: true, collection: 'foodcategory' })
-
-module.exports = mongoose.model('foodcategory', foodCategorySchema)
