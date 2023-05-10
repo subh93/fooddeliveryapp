0c70b3e440e2e55651ebfe1671907329ff2e9700
Subhashis Das <109717131+subh93@users.noreply.github.com>
2023-05-10 19:14:07 +0530

    Delete the older files

--- frontend/src/component/status/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Spinner from 'react-bootstrap/Spinner';
-
-export default function Loading() {
-    return (
-        <Spinner animation="border" variant="light" />
-    )
-}
